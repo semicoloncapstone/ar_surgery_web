d31db4cff0b6488dfda2a9ee3335368cf0a75b4f
bcassid5 <bcassid5@uwo.ca>
2018-01-23 16:09:59 -0500

    added footer

--- frontend/ar_surgery/app/components/home-page.js
+++ frontend/ar_surgery/app/components/home-page.js
@@ -6,32 +6,52 @@ export default Ember.Component.extend({
     isHistoryPage: false,
     isClassesPage: false,
     isSettingsPage: false,
+    Ho: "w3-blue",
+    Hi: "",
+    C: "",
+    S: "",
 
     actions: {
     
         HomePress () {
             this.set('isHomePage', true);
+            this.set('Ho', "w3-blue");
             this.set('isHistoryPage', false);
+            this.set('Hi', "");
             this.set('isClassesPage', false);
+            this.set('C', "");
             this.set('isSettingsPage', false);
+            this.set('S', "");
         },
         HistoryPress() {
             this.set('isHomePage', false);
+            this.set('Ho', "");
             this.set('isHistoryPage', true);
+            this.set('Hi', "w3-blue");
             this.set('isClassesPage', false);
+            this.set('C', "");
             this.set('isSettingsPage', false);
+            this.set('S', "");
         },
         ClassPress() {
             this.set('isHomePage', false);
+            this.set('Ho', "");
             this.set('isHistoryPage', false);
+            this.set('Hi', "");
             this.set('isClassesPage', true);
+            this.set('C', "w3-blue");
             this.set('isSettingsPage', false);
+            this.set('S', "");
         },
         SettingsPress() {
             this.set('isHomePage', false);
+            this.set('Ho', "");
             this.set('isHistoryPage', false);
+            this.set('Hi', "");
             this.set('isClassesPage', false);
+            this.set('C', "");
             this.set('isSettingsPage', true);
+            this.set('S', "w3-blue");
         },
 
     }
