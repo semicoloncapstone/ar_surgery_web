0eeed427ca0ad7b9a3cb1e41235ea0cb7e58112c
bcassid5 <bcassid5@uwo.ca>
2018-01-30 13:25:28 -0500

    edited messages page

--- frontend/ar_surgery/app/components/home-page.js
+++ frontend/ar_surgery/app/components/home-page.js
@@ -5,21 +5,25 @@ export default Ember.Component.extend({
     isHomePage: true, 
     isHistoryPage: false,
     isClassesPage: false,
+    isMessagesPage: false,
     isSettingsPage: false,
-    Ho: "w3-blue",
+    Ho: "w3-purple",
     Hi: "",
     C: "",
+    M: "",
     S: "",
 
     actions: {
     
         HomePress () {
             this.set('isHomePage', true);
-            this.set('Ho', "w3-blue");
+            this.set('Ho', "w3-purple");
             this.set('isHistoryPage', false);
             this.set('Hi', "");
             this.set('isClassesPage', false);
             this.set('C', "");
+            this.set('isMessagesPage', false);
+            this.set('M', "");
             this.set('isSettingsPage', false);
             this.set('S', "");
         },
@@ -30,6 +34,8 @@ export default Ember.Component.extend({
             this.set('Hi', "w3-blue");
             this.set('isClassesPage', false);
             this.set('C', "");
+            this.set('isMessagesPage', false);
+            this.set('M', "");
             this.set('isSettingsPage', false);
             this.set('S', "");
         },
@@ -39,7 +45,21 @@ export default Ember.Component.extend({
             this.set('isHistoryPage', false);
             this.set('Hi', "");
             this.set('isClassesPage', true);
-            this.set('C', "w3-blue");
+            this.set('C', "w3-red");
+            this.set('isMessagesPage', false);
+            this.set('M', "");
+            this.set('isSettingsPage', false);
+            this.set('S', "");
+        },
+        MessagePress() {
+            this.set('isHomePage', false);
+            this.set('Ho', "");
+            this.set('isHistoryPage', false);
+            this.set('Hi', "");
+            this.set('isClassesPage', false);
+            this.set('C', "");
+            this.set('isMessagesPage', true);
+            this.set('M', "w3-green");
             this.set('isSettingsPage', false);
             this.set('S', "");
         },
@@ -50,8 +70,10 @@ export default Ember.Component.extend({
             this.set('Hi', "");
             this.set('isClassesPage', false);
             this.set('C', "");
+            this.set('isMessagesPage', false);
+            this.set('M', "");
             this.set('isSettingsPage', true);
-            this.set('S', "w3-blue");
+            this.set('S', "w3-orange");
         },
 
     }
