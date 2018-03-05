03bafb1f21b1e72ff32dc27cdf1feba819bdea84
bcassid5 <bcassid5@uwo.ca>
2018-03-04 20:28:33 -0500

    attempting classes pull

--- frontend/ar_surgery/app/components/classes-page.js
+++ frontend/ar_surgery/app/components/classes-page.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    store: Ember.inject.service(),
+    classesModel: null,
+
+    init(){
+        this._super(...arguments);
+        var self = this;
+
+        this.get('store').findAll('class').then(function (records) {
+            self.set('classesModel', records);
+         });
+         console.log(this.get('classesModel'));
+    },
+
+    actions: {
+
+    }
 });
--- frontend/ar_surgery/app/components/home-page.js
+++ frontend/ar_surgery/app/components/home-page.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    store: Ember.inject.service(),
     routing: Ember.inject.service('-routing'),
     isHomePage: true, 
     isHistoryPage: false,
