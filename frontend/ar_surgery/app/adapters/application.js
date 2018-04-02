92d00bc864ea652b44c4faf68334a5db932e4f47
bcassid5 <bcassid5@uwo.ca>
2018-04-01 20:24:14 -0400

    revert to localhost addresses for testing

--- frontend/ar_surgery/app/adapters/application.js
+++ frontend/ar_surgery/app/adapters/application.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-    host: 'http://ec2-18-217-206-65.us-east-2.compute.amazonaws.com:3700'
+    host: 'http://localhost:3700'
 });
