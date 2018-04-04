2ca5889eada7d51043d8939cb26c89521fcd1583
bcassid5 <bcassid5@uwo.ca>
2018-04-03 22:37:08 -0400

    adjusted errors in low priority files

--- frontend/ar_surgery/app/helpers/if-cond.js
+++ frontend/ar_surgery/app/helpers/if-cond.js
@@ -3,25 +3,25 @@ import Ember from 'ember';
 export function ifCond([v1, condition, v2]) {
   switch (condition) {
     case '==':
-        return (v1 == v2)
+        return (v1 == v2);
     case '===':
-      return (v1 === v2)
+      return (v1 === v2);
     case '!=':
-        return (v1 != v2)
+        return (v1 != v2);
     case '!==':
-        return (v1 !== v2)
+        return (v1 !== v2);
     case '<':
-        return (v1 < v2)
+        return (v1 < v2);
     case '<=':
-        return (v1 <= v2)
+        return (v1 <= v2);
     case '>':
-        return (v1 > v2)
+        return (v1 > v2);
     case '>=':
-        return (v1 >= v2)
+        return (v1 >= v2);
     case '&&':
-        return (v1 && v2)
+        return (v1 && v2);
     case '||':
-        return (v1 || v2)
+        return (v1 || v2);
   }
 }
 
